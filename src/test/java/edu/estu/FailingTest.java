e65341b3607fecaf467cd0c95a947d47739725c1
Erdemtmztrk <147403255+Erdemtmztrk@users.noreply.github.com>
2024-01-06 22:39:56 +0300

    Update FailingTest.java

--- src/test/java/edu/estu/FailingTest.java
+++ src/test/java/edu/estu/FailingTest.java
@@ -2,16 +2,22 @@ package edu.estu;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-/* *********************************************************
- **** WRITE YOUR FAILING UNIT TEST(S) INTO  THIS FILE! *****
- ***********************************************************
- */
+/* *******************  ** WRITE YOUR FAILING UNIT TEST(S) INTO THIS FILE! **  ********************  */
 
-/**
- * Unit tests for the first part of the project.
- */
+/**  * Unit tests for the first part of the project.  */
 public class FailingTest {
+
+    @Test
+    public void testInfiniteLoopBug() {
+        
+        try {
+            App.infiniteLoop(Double.POSITIVE_INFINITY);
+            fail("Expected an infinite loop, but the method completed successfully.");
+        } catch (Exception e) {
+           
+        }
+    }
 
 }
